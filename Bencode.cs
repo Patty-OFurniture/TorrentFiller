7832bcce856026a0686d1f85407c4f0ebc7e6b70
Patty-OFurniture <127074553+Patty-OFurniture@users.noreply.github.com>
2023-07-28 10:48:45 -0400

    Add files via upload
    Initial tests

---
+++ Bencode.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torrent
+{
+    public struct TorrentData
+    {
+        public string announce;
+        public List<string> announce_list;
+        public DateTime creation_date;
+        public TorrentInfo info;
+    }
+
+    public struct TorrentFile
+    {
+        public int length;
+        public string path;
+    }
+
+    public struct TorrentInfo
+    {
+        public string name;
+        public int piece_length;
+        public int is_private;
+        public List<TorrentFile> files;
+        public List<string> pieces; // 20-byte SHA1 hash
+    }
+
+    public struct FileHash
+    {
+        // [info] -- TorrentInfo.files[].TorrentFile.length
+        public int FileLength;
+        // [info] -- TorrentInfo.files[].TorrentFile.path
+        public string Path;
+        // offset into the file where the first PieceHash starts
+        public int HashOffset;
+        // [piece length] -- TorrentInfo.piece_length
+        public int PieceLength;
+        // [pieces] -- lowercase SHA-1
+        public List<string> PieceHashes;
+    }
+
+    public class Bencode : IDisposable
+    {
+        private Stream stream;
+        private string path;
+        private int indent;
+
+        public Bencode(string _path)
+        {
+            stream = null;
+            path = _path;
+            indent = 0;
+        }
+
+        // this throws away hashes that span files
+        public List<FileHash> CreateFileHashList(TorrentData data)
+        {
+            List<FileHash> fileHashList = new List<FileHash>();
+
+            if (data.info.files == null)
+                return fileHashList;
+
+            int fileIndex = 0;
+            int fileOffset = 0;
+            int fileRemainder = data.info.files[fileIndex].length;
+
+            for (int pieceIndex = 0; pieceIndex < data.info.pieces.Count; pieceIndex++)
+            {
+                FileHash fileHash = new FileHash();
+                fileHash.Path = data.info.files[fileIndex].path;
+                fileHash.FileLength = data.info.files[fileIndex].length;
+                fileHash.HashOffset = fileOffset;
+                fileHash.PieceHashes = new List<string>();
+
+                while (fileRemainder >= data.info.piece_length)
+                {
+                    fileOffset += data.info.piece_length;
+                    fileRemainder -= data.info.piece_length;
+                    fileHash.PieceHashes.Add(data.info.pieces[pieceIndex++]);
+                }
+                fileHashList.Add(fileHash);
+
+                while (++fileIndex < data.info.files.Count)
+                {
+                    if (fileRemainder >= data.info.files[fileIndex].length)
+                        fileRemainder -= data.info.files[fileIndex].length;
+                    else
+                        break; // goto might be harmless here
+                }
+
+                if (!(fileIndex < data.info.files.Count))
+                    return fileHashList;
+
+                // 10k remainder with 32k pieces means skip 22k before hashing
+                fileOffset = data.info.piece_length - fileRemainder;
+                fileRemainder = data.info.files[fileIndex].length - fileOffset;
+            }
+
+            return fileHashList;
+        }
+
+        private List<TorrentFile> CreateTorrentFileList(List<object> l)
+        {
+            var result = new List<TorrentFile>();
+
+            if (l != null)
+            {
+                foreach(var i in l)
+                {
+                    if (i is IDictionary<string, object>)
+                    {
+                        IDictionary<string, object> d = (IDictionary<string, object>)i;
+                        var t = new TorrentFile();
+                        t.length = Convert.ToInt32(d["length"]);
+                        t.path = d["path"].ToString();
+                        result.Add(t);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private List<string> CreateTorrentPieceInfo(byte[] pieces)
+        {
+            var pieceList = new List<string>();
+            var sBuilder = new StringBuilder();
+
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                sBuilder.Append(pieces[i].ToString("x2"));
+                if (sBuilder.Length == 40)
+                {
+                    // Console.WriteLine(new string(' ', indent) + sBuilder.ToString());
+                    pieceList.Add(sBuilder.ToString());
+                    sBuilder = new StringBuilder();
+                }
+            }
+            return pieceList;
+        }
+
+        private TorrentInfo CreateTorrentInfo(IDictionary<string, object> d)
+        {
+            string key;
+            var torrentInfo = new TorrentInfo();
+
+            if (d != null)
+            {
+                key = "name";
+                if (d.ContainsKey(key))
+                    torrentInfo.name = d[key].ToString();
+
+                key = "piece length";
+                if (d.ContainsKey(key))
+                    torrentInfo.piece_length = (int) d[key];
+
+                key = "pieces";
+                if (d.ContainsKey(key))
+                    torrentInfo.pieces = CreateTorrentPieceInfo(d[key] as byte[]);
+
+                key = "private";
+                if (d.ContainsKey(key))
+                    torrentInfo.is_private = (int)d[key];
+
+                key = "files";
+                if (d.ContainsKey(key))
+                    torrentInfo.files = CreateTorrentFileList(d[key] as List<object>);
+            }
+
+            return torrentInfo;
+        }
+
+        public TorrentData CreateTorrentData(IDictionary<string, object> d)
+        {
+            string key;
+            var torrentData = new TorrentData();
+
+            if (d != null)
+            {
+                key = "announce";
+                if (d.ContainsKey(key))
+                    torrentData.announce = d[key].ToString();
+
+                key = "announce-list";
+                if (d.ContainsKey(key))
+                {
+                    torrentData.announce_list = new List<string>();
+                    foreach(var s in d[key] as List<object>)
+                    {
+                        torrentData.announce_list.Add(s.ToString());
+                    }
+                    
+                }
+
+                key = "creation date";
+                if (d.ContainsKey(key))
+                    torrentData.creation_date = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(d[key])).DateTime;
+
+                key = "info";
+                if (d.ContainsKey(key))
+                    torrentData.info = CreateTorrentInfo(d[key] as IDictionary<string, object>);
+            }
+
+            return torrentData;
+        }
+
+        public bool OpenTorrent()
+        {
+            bool status = false;
+
+            try
+            {
+                stream = System.IO.File.OpenRead(path);
+                status = true;
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return status;
+        }
+
+        private Dictionary<string, object> ReadDictionary()
+        {
+            var result = new Dictionary<string, object>();
+            while (true)
+            {
+                object key = Read();
+                if (key == null)
+                    break;
+                object value = Read();
+                if (value == null)
+                    break;
+
+                result.Add(key.ToString(), value);
+            }
+            return result;
+        }
+
+        private object ReadList()
+        {
+            List<object> items = new List<object>();
+            object item = Read();
+            while (item != null)
+            {
+                if (item is string)
+                    items.Add(item.ToString());
+                else
+                    items.Add(item);
+
+                item = Read();
+            }
+
+            if (items.Count == 1)
+                return items[0];
+            else
+                return items;
+        }
+
+        private int ReadInt()
+        {
+            StringBuilder sb = new StringBuilder();
+            int c = stream.ReadByte();
+
+            do
+            {
+                sb.Append((char)c);
+                c = stream.ReadByte();
+            }
+            while (c != 'e');
+
+            return Convert.ToInt32(sb.ToString());
+        }
+
+        private object ReadString(int c)
+        {
+            bool isAscii = true;
+
+            StringBuilder sb = new StringBuilder();
+            while (c != ':')
+            {
+                sb.Append((char)c);
+                c = stream.ReadByte();
+            }
+
+            int stringLength = Convert.ToInt32(sb.ToString());
+            byte[] bytes = new byte[stringLength];
+
+            for (int i = 0; i <  stringLength; i++)
+            {
+                char ch = (char) stream.ReadByte();
+                if (!char.IsLetterOrDigit(ch) && !char.IsPunctuation(ch) && !char.IsWhiteSpace(ch))
+                    isAscii = false;
+
+                bytes[i] = (byte) ch;
+            }
+
+            if (isAscii)
+            {
+                string result = System.Text.Encoding.UTF8.GetString(bytes);
+                return result;
+            }
+
+            return bytes;
+        }
+
+        public object Read()
+        {
+            object result = null;
+
+            while (true)
+            {
+                int c = stream.ReadByte();
+                if (c < 0)
+                    return null;
+
+                Console.Write(new string(' ', indent));
+                switch (c)
+                {
+                    case 'e':
+                        indent -= 2;
+                        Console.WriteLine("End");
+                        break;
+                    case 'd':
+                        indent += 2;
+                        Console.WriteLine("Dictionary");
+                        result = ReadDictionary();
+                        break;
+                    case 'l':
+                        indent += 2;
+                        Console.WriteLine("List");
+                        result = ReadList();
+                        break;
+                    case 'i':
+                        Console.WriteLine(result = ReadInt());
+                        break;
+                    default:
+                        Console.WriteLine(result = ReadString(c));
+                        break;
+                }
+                return result;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (stream != null)
+                stream.Dispose();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using XSystem.Security.Cryptography;
+
+internal class Program
+{
+    private static int pieceLength = 1024 * 32; // default, will be set from .torrent
+    private static string searchRoot = @"D:\Downloads\celebs\A-J\Hayden Panettiere";
+
+    private static void Main(string[] args)
+    {
+        var torrentFiles = Directory.GetFiles(searchRoot, "*.torrent", SearchOption.AllDirectories);
+        foreach (var torrentFile in torrentFiles)
+        {
+            ParseTorrent(torrentFile);
+        }
+
+        // unit test
+        //int fileSize = 52624;
+        //int offset = 6081;
+        //var fileName = FindFile(searchRoot, pieceLength, offset, fileSize);
+        //Console.WriteLine(fileName);
+    }
+
+    static bool ParseTorrent(string torrentFile)
+    {
+        bool result = false;
+        try
+        {
+            var reader = new Torrent.Bencode(torrentFile);
+            if (reader.OpenTorrent())
+            {
+                var d = reader.Read() as IDictionary<string, object>;
+                var t = reader.CreateTorrentData(d);
+                var h = reader.CreateFileHashList(t);
+
+                pieceLength = t.info.piece_length;
+
+                foreach (var v in h)
+                {
+                    Console.WriteLine($"{Environment.NewLine}{v.Path} {v.FileLength} {v.HashOffset}");
+                    foreach (var z in v.PieceHashes)
+                    {
+                        Console.WriteLine(z);
+                    }
+
+                    var fileName = FindFile(searchRoot, pieceLength, v.HashOffset, v.FileLength);
+                    if (!string.IsNullOrEmpty(fileName))
+                    {
+                        Console.WriteLine($"Found: {fileName}");
+                        string destination = Path.Combine(t.info.name, v.Path);
+                        
+                        if (!Directory.Exists(t.info.name))
+                            Directory.CreateDirectory(t.info.name);
+
+                        System.IO.File.Copy(fileName, destination, false);
+                    }
+                }
+            }
+            result = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        return result;
+    }
+
+    static string FindFile(string root, int pieceLength, int offset, int fileSize)
+    {
+        string result = "";
+
+        var files = Directory.GetFiles(root, "*", SearchOption.AllDirectories);
+        foreach (var file in files)
+        {
+            var fileInfo = new FileInfo(file);
+            if (fileInfo.Length == fileSize)
+            {
+                var buffer = new byte[pieceLength];
+
+                using (var stream = File.OpenRead(file))
+                using (var reader = new BinaryReader(stream))
+                {
+                    if (offset > 0)
+                        reader.Read(buffer, 0, offset);
+
+                    while (pieceLength == reader.Read(buffer, 0, pieceLength))
+                    {
+                        var hash = Hash(buffer);
+                        Console.WriteLine(hash);
+                    }
+                }
+
+                result = file;
+            }
+        }
+        return result;
+    }
+
+    static string Hash(byte[] input)
+    {
+        using (SHA1Managed sha1 = new SHA1Managed())
+        {
+            // convert char[] from StreamReader to byte[]
+            var hash = sha1.ComputeHash(input);
+            var sb = new StringBuilder(hash.Length * 2);
+
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
