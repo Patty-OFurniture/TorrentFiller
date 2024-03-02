7f8eff9d8236dfbf5cb70b3d099dc1d7db41934a
Patty-OFurniture <127074553+Patty-OFurniture@users.noreply.github.com>
2024-03-02 10:07:37 -0500

    Logging / progress bar fixes

---
+++ UI/TextProgressBar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTester.UI
+{
+    // modified from https://www.codeproject.com/Tips/5255878/A-Console-Progress-Bar-in-Csharp
+    internal class TextProgressBar 
+    {
+        const char _block = '■';
+        const string _twirl = "-\\|/";
+        private string _back;
+        private decimal _widthChars;
+
+        public TextProgressBar(decimal widthChars=20)
+        {
+            _widthChars = widthChars;
+            _back = new string('\b', (int) Math.Round(_widthChars));
+        }
+
+        public void WriteProgressBar(decimal percent, bool update = false)
+        {
+            if (update)
+                Console.Write(_back);
+            Console.Write("[");
+            var p = (int)((percent * _widthChars / 100m) + .5m);
+            for (var i = 0; i < _widthChars; ++i)
+            {
+                if (i >= p)
+                    Console.Write(' ');
+                else
+                    Console.Write(_block);
+            }
+            Console.Write("] {0,3:##0}%", percent);
+        }
+        public void WriteProgress(int progress, bool update = false)
+        {
+            int x,y;
+            (x, y) = Console.GetCursorPosition();
+
+            if (update)
+                Console.Write("\b");
+            Console.Write(_twirl[progress % _twirl.Length]);
+
+            Console.SetCursorPosition(x, y);
+        }
+    }
+}
---
+++ UI/UI.cs
@@ -0,0 +1,67 @@
+﻿// use code from Simon Mourier
+// https://stackoverflow.com/questions/11624298/how-do-i-use-openfiledialog-to-select-a-folder/66187224#66187224
+#define UI_CUSTOM
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+
+namespace HashTester.UI
+{
+    internal class UI
+    {
+#if UI_WINDOWS && UI_CUSTOM
+        public static string? RequestFolder(string? InputPath = null)
+        {
+            string? result = null;
+            var dlg = new FolderPicker();
+            //dlg.InputPath = @"c:\windows\system32";
+            if (dlg.ShowDialog(IntPtr.Zero) == true)
+            {
+                result = dlg.ResultPath;
+            }
+            return result;
+        }
+
+#elif UI_WINDOWS
+        public static string? RequestFolder()
+        {
+            string? result = null;
+
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Select Folder";
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                result = Path.GetDirectoryName(folderBrowser.FileName);
+            }
+            return result;
+        }
+#elif UI_CONSOLE
+        public static string? RequestFolder()
+        {
+            string? result = null;
+
+            do
+            {
+                // write to stderror in case stdout is piped
+                Console.Error.WriteLine("Folder to examine:");
+                result = Console.ReadLine();
+            }
+            while (!Directory.Exists(result));
+
+            return result;
+        }
+#endif
+    }
+}
